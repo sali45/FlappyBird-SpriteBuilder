609f716ee58678b7e52453cfa3afe7c18b07151e
Saqib Ali <saqibali@Saqibs-MacBook-Pro.local>
2014-12-17 16:26:59 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,11 +9,20 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f) {
+        [self addObstacle];
+        
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
